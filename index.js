eb682746b990119a93843e19d60e1e0174cb478b
Teus369 <mateusizaias369@gmail.com>
2023-07-05 21:16:58 -0300

    atualização pagina status

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 // Lógica do carrossel
-var currentImage = 0;
-var images = document.getElementsByClassName('carrossel-image');
-var captions = document.getElementsByClassName('carrossel-caption')[0];
-var navigationButtons = document.getElementsByClassName('carrossel-navigation')[0];
+let currentImage = 0;
+const images = document.querySelectorAll('.carrossel-image');
+const captions = document.querySelector('.carrossel-caption');
+const navigationButtons = document.querySelector('.carrossel-navigation');
 
 function createNavigationButtons() {
-  for (var i = 0; i < images.length; i++) {
-    var button = document.createElement('button');
-    button.setAttribute('onclick', 'showImage(' + i + ')');
+  for (let i = 0; i < images.length; i++) {
+    const button = document.createElement('button');
+    button.addEventListener('click', () => showImage(i));
     navigationButtons.appendChild(button);
   }
 }
 
 function updateNavigationButtons() {
-  var buttons = navigationButtons.getElementsByTagName('button');
-  for (var i = 0; i < buttons.length; i++) {
+  const buttons = navigationButtons.getElementsByTagName('button');
+  for (let i = 0; i < buttons.length; i++) {
     buttons[i].classList.remove('active');
   }
   buttons[currentImage].classList.add('active');
 }
 
 function showImage(n) {
-  // Esconde todas as imagens
-  for (var i = 0; i < images.length; i++) {
-    images[i].classList.remove('active');
+  if (n < 0 || n >= images.length) {
+    return; // Verifica se o índice está dentro dos limites
   }
-  
+
+  // Esconde todas as imagens
+  images.forEach(image => image.classList.remove('active'));
+
   // Mostra a imagem atual
   images[n].classList.add('active');
   currentImage = n;
-  
+
   // Atualiza os botões de navegação
   updateNavigationButtons();
-  
+
   // Atualiza o texto da legenda
-  var captionText = images[n].querySelector('.carrossel-caption').innerHTML;
+  const captionText = images[n].querySelector('.carrossel-caption').innerHTML;
   captions.innerHTML = captionText;
 }
 
@@ -55,6 +57,4 @@ createNavigationButtons();
 showImage(currentImage);
 
 // Reprodução automática
-setInterval(function() {
-  nextImage();
-}, 30000);
+setInterval(nextImage, 5000);
